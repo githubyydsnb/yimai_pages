d0cc4e63b6fe03450908d610ef061d8147275b2f
rachel32112 <3118695070@qq.com>
2024-03-26 09:20:09 +0800

    3.25 收藏+地址

---
+++ api/collect.js
@@ -0,0 +1,28 @@
+import request from "../utils/request.js";
+
+const baseUrl = '/collect';
+
+
+const getCollectionsCount = () => {
+    return request.post(baseUrl + "/getCollectionsCount");
+}
+const getCollectionsList = () => {
+    return request.post(baseUrl + "/getCollectionsList");
+}
+const delCollections = (product) => {
+    return request.post(baseUrl + "/delCollections",{
+        product:product
+    });
+}
+const addCollection = (product) => {
+    return request.post(baseUrl + "/addCollections",{
+        product:product
+    });
+}
+
+export {
+    getCollectionsCount,
+    getCollectionsList,
+    delCollections,
+    addCollection,
+}
